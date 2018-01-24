40bc8f1cc9b9d9f72f6fe2216471e675d1eb6cb6
antoine <pelletant.antoine@gmail.com>
2018-01-24 02:03:19 +0100

    basics of the header files

---
+++ include/malloc.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** my_malloc
+** File description:
+** malloc header file
+*/
+
+#ifndef MALLOC_H_
+#define MALLOC_H_
+
+
+
+#endif /* MALLOC_H_ */
---
+++ include/realloc.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** malloc
+** File description:
+** realoc header file
+*/
+
+#ifndef REALLOC_H_
+#define REALLOC_H_
+
+
+
+#endif /* REALLOC_H_ */
