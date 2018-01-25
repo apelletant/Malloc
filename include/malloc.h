f02bf4001be2810612d310b7d6f72851b9bc08fb
antoine <pelletant.antoine@gmail.com>
2018-01-25 18:45:55 +0100

    first thing to malloc

--- include/malloc.h
+++ include/malloc.h
@@ -6,8 +6,19 @@
 */
 
 #ifndef MALLOC_H_
-#define MALLOC_H_
+	#define MALLOC_H_
 
+	#include <stddef.h>
 
+	typedef struct s_block
+	{
+		size_t size;
+		int free;
+	        struct s_block *next;
+		struct s_block *prev;
+	} t_block;
 
+	#ifndef BLOCK_SIZE_
+		#define BLOCK_SIZE sizeof(t_block)
+	#endif /* BLOC_SIZE_ */
 #endif /* MALLOC_H_ */
--- src/malloc.c
+++ src/malloc.c
@@ -5,3 +5,48 @@
 ** malloc source code
 */
 
+#include <stddef.h>
+#include <unistd.h>
+#include "malloc.h"
+
+t_block *find_free_space(t_block *head, t_block **last_block, size_t size)
+{
+	t_block *tmp = head;
+
+	while (tmp) {
+		if (tmp->free && tmp->size >= size) {
+			return (tmp);
+		}
+		tmp->prev = tmp;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+t_block *get_more_space(t_block *last_block, size_t size)
+{
+	t_block *new_block = NULL;
+	void *request;
+
+	new_block = sbrk(0);
+	request = sbrk(size + BLOCK_SIZE);
+	if (request == (void *) -1)
+		return (NULL);
+	if (last_block) {
+		last_block->prev = last_block;
+		last_block->next = new_block;
+	}
+        new_block->size = size;
+	new_block->next = NULL;
+	new_block->prev = last_block;
+	new_block->free = 0;
+	return (new_block);
+}
+
+void	*malloc(size_t size)
+{
+	
+	if (size == 0)
+		return NULL;
+	return (ptr);
+}
