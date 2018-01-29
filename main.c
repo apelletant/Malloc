e998a0aac24acebd9de8a9c7f9620d2767099e18
Invicta <invicta@localhost.localdomain>
2018-01-29 14:22:22 +0100

    test main

--- main.c
+++ main.c
@@ -2,5 +2,34 @@
 ** EPITECH PROJECT, 2018
 ** main test
 ** File description:
-** test man for the malloc library
+** test main for the malloc library
 */
+
+#include <stdio.h>
+#include "malloc.h"
+
+int main()
+{
+	int i = 0;
+	char **tab;
+
+	tab = malloc(sizeof(char *) * 5);
+	tab[0] = malloc(sizeof(char) * (7 + 1));
+	tab[0] = "bonjour";
+	tab[1] = malloc(sizeof(char) * (3 + 1));
+	tab[1] = "les";
+	tab[2] = malloc(sizeof(char) * (4 + 1));
+	tab[2] = "gens";
+	tab[3] = malloc(sizeof(char) * (7 + 1));
+	tab[3] = "Antoine";
+	tab[4] = malloc(sizeof(char) * (7 + 1));
+	tab[4] = "Kellian";
+
+	while (i < 5) {
+		printf("%s\n", tab[i]);
+		free(tab[i]);
+		++i;
+	}
+	free(tab);
+	return (0);
+}
