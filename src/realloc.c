d53bc3edd4d2521c90ec9f396cfa1835805f9a6c
antoine <pelletant.antoine@gmail.com>
2018-02-06 11:40:16 +0100

    added thread in realloc

--- src/realloc.c
+++ src/realloc.c
@@ -5,45 +5,33 @@
 ** realloc function
 */
 
-#include <unistd.h>
-
 #include <string.h>
+#include <pthread.h>
 #include "malloc.h"
+
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *realloc(void *ptr, size_t size)
 {
-/*	void *tmp;
-	block_t *new_block = NULL;
-
-	if (!ptr)
-		return (malloc(size));
-	if (size == 0 && ptr != NULL) {
-		free(ptr);
-		return (NULL);
-	}
-	new_block = (block_t*)ptr - 1;
-	if (new_block->size >= size)
-		return (ptr);
-	tmp = malloc(size);
-	if (tmp) {
-		memcpy(tmp, ptr, new_block->size);
-		free(ptr);
-	}
-	return (tmp);*/
 	block_t *block_tmp = (block_t *)ptr -1;
         void *new_ptr = NULL;
 
+	pthread_mutex_lock(&mutex);
 	if (!ptr) {
+		pthread_mutex_unlock(&mutex);
 		return (malloc(size));
 	}
 	if (block_tmp->size >= size) {
+		pthread_mutex_unlock(&mutex);
 		return block_tmp;
 	}
 	new_ptr = malloc(size);
 	if (!new_ptr) {
+		pthread_mutex_unlock(&mutex);
 		return (NULL);
 	}
 	memcpy(new_ptr, ptr, block_tmp->size);
 	free(ptr);
+	pthread_mutex_unlock(&mutex);
 	return (new_ptr);
 }
